a2acfdb12e7424999ef63966bd5689915d98d88b
osman <osmanhussein@live.nl>
2023-03-17 00:11:23 +0100

    Add project files.

---
+++ terminal-switcher-backend/Controllers/CommandController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace terminal_switcher_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        
+        // GET api/<CommandController>/5
+        [HttpGet("cmd")]
+        public async Task<string> get(string command)
+        {
+            return await new command().DockerExec(command);
+        }
+    }
+}
---
+++ terminal-switcher-backend/command.cs
@@ -0,0 +1,52 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace terminal_switcher_backend
+{
+    public class command
+    {
+        private DockerClient client;
+
+        public command() 
+        {
+            client = new DockerClientConfiguration().CreateClient();
+            
+        }        
+        
+        public async Task<string> DockerExec(string command)
+        {
+            List<string> commands = command.Split(' ').ToList();
+
+            var execCreateParameters = new ContainerExecCreateParameters
+            {
+                Cmd = commands,
+                AttachStdout = true,
+                AttachStderr = true,
+            };
+
+            var execCreateResponse = await client.Exec.ExecCreateContainerAsync("ubuntu", execCreateParameters, default(CancellationToken));
+
+            using (var stdOutAndErrStream = await client.Exec.StartAndAttachContainerExecAsync(execCreateResponse.ID, false, default(CancellationToken)))
+            {
+                var (stdout, stderr) = await stdOutAndErrStream.ReadOutputToEndAsync(default(CancellationToken));
+                return stdout.ToString();
+            }
+        }
+
+
+        public string sendCommand(string command) 
+        {
+            if(command == "id")
+            {
+                return "osman";
+            }
+
+            if(command == "ls")
+            {
+                return "list of files";
+            }
+
+            return "unkown command";
+        }
+    }
+}
