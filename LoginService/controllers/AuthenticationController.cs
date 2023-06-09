eab2f56dcd0efb84d0f26508d1079e079293f9ef
OsmanHussein123 <36198414+OsmanHussein123@users.noreply.github.com>
2023-06-09 10:51:53 +0200

    1q1

--- LoginService/controllers/AuthenticationController.cs
+++ LoginService/controllers/AuthenticationController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using BCrypt.Net;
 using System.Security.Claims;
 
 namespace LoginService.controllers
